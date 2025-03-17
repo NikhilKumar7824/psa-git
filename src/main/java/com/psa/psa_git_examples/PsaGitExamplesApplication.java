ec6bd0e4a9abe0e39e7d5591102eaecc73baeb20
NikhilKumar7824 <116564547+NikhilKumar7824@users.noreply.github.com>
2025-03-17 13:32:20 +0530

    Update PsaGitExamplesApplication.java

--- src/main/java/com/psa/psa_git_examples/PsaGitExamplesApplication.java
+++ src/main/java/com/psa/psa_git_examples/PsaGitExamplesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PsaGitExamplesApplication {
 
 	public static void main(String[] args) {
+		char x = 'a';
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
 
